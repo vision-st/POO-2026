9c404bed45d72de379e4ead112b3daad183f784c
Jorge Carmona <jorgecarmona1986@gmail.com>
2026-06-03 00:39:36 -0400

    kill

--- semana3espejo/RobotsSemana3/src/main/java/com/duoc/fabricarobots/exceptions/RutinvalidoException.java
+++
@@ -1,9 +0,0 @@
-package com.duoc.fabricarobots.exceptions;
-
-public class RutinvalidoException extends Exception {
-
-    public RutinvalidoException(String mensaje){
-        super(mensaje);
-    }
-
-}
